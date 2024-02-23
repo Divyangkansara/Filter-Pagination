478432a6024bf403557484470d0a6673b3d75a4d
Divyang <divyang.kansara@technostacks.com>
2024-02-23 16:53:22 +0530

    final commit

---
+++ myapp/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from .models import Person
+from .filters import PersonFilter
+from django.core.paginator import Paginator
+
+# Create your views here.
+def show_person_page(request):
+    context = {}
+
+    filtered_persons = PersonFilter(
+        request.GET,
+        queryset=Person.objects.all()
+    )
+    # print(filtered_persons)
+
+    context['filtered_persons'] = filtered_persons
+    paginated_filtered_persons = Paginator(filtered_persons.qs, 2)
+    page_number = request.GET.get('page')
+    person_page_obj = paginated_filtered_persons.get_page(page_number)
+    context['person_page_obj'] = person_page_obj
+    return render(request, 'myapp/persons.html', context)
